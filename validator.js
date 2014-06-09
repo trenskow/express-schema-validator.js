a1f658e622f4f11599eae062246645d6cfecf48d
Kristian Trenskow <trenskow@me.com>
2014-06-09 05:44:45 +0200

    Changed warning and the way it is written.

--- validator.js
+++ validator.js
@@ -1,23 +1,43 @@
 var validate = require('isvalid-express');
 
-console.log('*****************************************************');
-console.log('*            express-schema-validator.js            *');
-console.log('*****************************************************');
-console.log('*            This library is depricated.            *');
-console.log('*                                                   *');
-console.log('*        This library has been forked into          *');
-console.log('*                isvalid-express.js                 *');
-console.log('*                       and                         *');
-console.log('*                    isvalid.js                     *');
-console.log('*                                                   *');
-console.log('*                      Go to                        *');
-console.log('*  https://github.com/trenskow/isvalid-express.js   *');
-console.log('*               for more information                *');
-console.log('*                                                   *');
-console.log('*    This library now uses the above libraries.     *');
-console.log('* But for future reference update you dependencies  *');
-console.log('*                   and your code.                  *');
-console.log('*****************************************************');
+var warning = [
+'-',
+'express-schema-validator.js',
+'-',
+'This library is depricated.',
+'',
+'This library has been forked into',
+'isvalid-express.js',
+'and',
+'isvalid.js',
+'',
+'Go to',
+'https://github.com/trenskow/express-schema-validator.js',
+'for more information',
+'',
+'This library now uses the above libraries.',
+'But for future reference update your dependencies',
+'and your code.',
+'-'
+];
+
+var len = 2;
+for (var idx = 0 ; idx < warning.length ; idx++) len = Math.max(len, warning[idx].length + 2);
+
+console.log();
+for (var idx = 0 ; idx < warning.length ; idx++) {
+	var str = '*';
+	if (warning[idx] == '-') str += new Array(len + 1).join('*');
+	else {
+		var spacing = len - warning[idx].length;
+		str += new Array(Math.floor(spacing / 2) + 1).join(' ');
+		str += warning[idx];
+		str += new Array(Math.ceil(spacing / 2) + 1).join(' ');
+	}
+	str += '*';
+	console.log(str);
+}
+console.log();
 
 module.exports.validateBody = validate.body;
 module.exports.validateQuery = validate.query;
